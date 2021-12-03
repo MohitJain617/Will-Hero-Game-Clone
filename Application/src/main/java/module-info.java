ab1bb3286df2c7ea7d3f44f3144fe819de61395f
hp <sanyam20116@iiitd.ac.in>
2021-12-03 16:54:28 +0530

    Images added

--- Application/src/main/java/module-info.java
+++ Application/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.example.application {
     requires javafx.controls;
     requires javafx.fxml;
-
-
     opens com.example.application to javafx.fxml;
     exports com.example.application;
 }
