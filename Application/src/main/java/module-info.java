8e015e48040ffbe61d827488df38a831fa47909f
hp <sanyam20116@iiitd.ac.in>
2021-12-04 19:36:58 +0530

    Menu Scene Builder

--- Application/src/main/java/module-info.java
+++ Application/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.application {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     opens com.example.application to javafx.fxml;
     exports com.example.application;
 }
