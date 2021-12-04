901cc7f62658eabb776e53e3ecf1bb81e08358c5
Mohit Jain x64 <mohit20221@iiitd.ac.in>
2021-12-04 22:37:48 +0530

    Giving functionality to buttons

--- Application/src/main/java/com/example/application/Game.java
+++ Application/src/main/java/com/example/application/Game.java
@@ -39,6 +39,20 @@ public class Game extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    public void showGamePlay(ActionEvent e) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GamePlay.fxml")));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void showEndMenu(ActionEvent e) throws IOException {
+        Parent root2 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("EndGameMenu.fxml")));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root2);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch();
