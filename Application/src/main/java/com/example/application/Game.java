cfe30fdb7d3324c853d9f3231de25a208b904c0e
hp <sanyam20116@iiitd.ac.in>
2021-12-07 16:10:46 +0530

    Introscene

--- Application/src/main/java/com/example/application/Game.java
+++ Application/src/main/java/com/example/application/Game.java
@@ -21,30 +21,34 @@ public class Game extends Application {
     private Parent root;
     private GamePlay gameplay;
     private Image icon ;
-    //-------------------------
+
     public Game(){
         gameplay = new GamePlay();
         icon = new Image("hero.png");
     }
+
     @Override
     public void start(Stage stage) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainMenu.fxml")));
-        root.setOpacity(0);
+
         this.stage = stage ;
-        stage.setTitle("WillHero");
-        scene = new Scene(root);
+        stage.setTitle("Will Hero");
         stage.getIcons().add(icon);
 
-        Parent root2 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Intro.fxml")));
-        Scene introscene = new Scene(root2);
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainMenu.fxml")));
+        root.setOpacity(0.2);
+        scene = new Scene(root,900,560);
 
-        stage.setScene(introscene);
+        ShowGameIntro();
+    }
+
+    private void ShowGameIntro() throws IOException {
+
+        Parent introRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Intro.fxml")));
+        Scene introScene = new Scene(introRoot,900,560);
+        stage.setScene(introScene);
         stage.show();
-        makeFadeout(root2);
 
-        //PauseTransition del = new PauseTransition(Duration.seconds(1));
-//        del.setOnFinished(event -> stage.setScene(scene));
-//        del.play();
+        makeFadeout(introScene);
     }
 
     public void showMainMenu(ActionEvent e) throws IOException {
@@ -79,14 +83,15 @@ public class Game extends Application {
         stage.show();
     }
 
-    private void makeFadeout(Parent node){
+    private void makeFadeout(Scene fadeScene){
+
         FadeTransition fade = new FadeTransition();
         fade.setDuration(Duration.millis(1000));
-        fade.setNode(node);
+        fade.setNode(fadeScene.getRoot());
         fade.setFromValue(1);
-        fade.setToValue(0);
+        fade.setToValue(0.4);
         fade.setOnFinished((ActionEvent event)-> {
-            this.stage.setScene(scene);
+            this.stage.setScene(this.scene);
             makeFadeIn();
         });
         fade.play();
@@ -95,13 +100,13 @@ public class Game extends Application {
     private void makeFadeIn(){
         FadeTransition fade = new FadeTransition();
         fade.setDuration(Duration.millis(1000));
-        fade.setNode(root);
-        fade.setFromValue(0);
+        fade.setNode(this.root);
+        fade.setFromValue(0.2);
         fade.setToValue(1);
         fade.play();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
