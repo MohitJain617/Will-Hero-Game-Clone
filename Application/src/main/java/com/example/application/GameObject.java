d9554efda6f39bc7306368c5daac08f4b8d53053
Mohit Jain x64 <mohit20221@iiitd.ac.in>
2021-12-09 16:38:15 +0530

    Implementing collision checker

--- Application/src/main/java/com/example/application/GameObject.java
+++ Application/src/main/java/com/example/application/GameObject.java
@@ -6,8 +6,8 @@ import javafx.scene.layout.AnchorPane;
 import java.io.Serializable;
 
 public abstract class GameObject implements Serializable {
-    private Location loc;
-    private Boundary bounds;
+    private final Location loc;
+    private final Boundary bounds;
     public GameObject(double x, double y, double xwidth, double ywidth){
         loc = new Location(x,y);
         bounds = new Boundary(xwidth,ywidth);
@@ -20,10 +20,33 @@ public abstract class GameObject implements Serializable {
         loc.setY(y);
     }
     public Location getLocation(){
-        Location l2 = new Location(loc.getX(),loc.getY());
-        return l2;
+        return new Location(loc.getX(),loc.getY());
     }
-    public boolean checkCollision(GameObject g1){
-        return false;
+    public Boundary getBounds(){
+        return new Boundary(bounds.getX(),bounds.getY());
+    }
+    public boolean checkCollision(GameObject gob){
+        Location l2 = gob.getLocation();
+        Boundary b2 = gob.getBounds();
+        //gobs bounds
+        double yb1,yb2,xb1,xb2;
+        yb1 = l2.getY()-b2.getY()/2;
+        yb2 = l2.getY()+b2.getY()/2;
+        xb1 = l2.getX()-b2.getX()/2;
+        xb2 = l2.getX()+b2.getX()/2;
+        //current objects bounds
+        double cyb1,cyb2,cxb1,cxb2;
+        cyb1 = loc.getY()-bounds.getY()/2;
+        cyb2 = loc.getY()+bounds.getY()/2;
+        cxb1 = loc.getX()-bounds.getX()/2;
+        cxb2 = loc.getX()+bounds.getX()/2;
+        //check collision
+        boolean ymatch = false;
+        boolean xmatch = false;
+        if((yb1 >= cyb1) && (yb1 <= cyb2)) ymatch = true;
+        if((yb2 >= cyb1) && (yb2 <= cyb2)) ymatch = true;
+        if((xb1 >= cxb1) && (xb1 <= cxb2)) xmatch = true;
+        if((xb2 >= cxb1) && (xb2 <= cxb2)) xmatch = true;
+        return (ymatch && xmatch);
     }
 }
---
+++ Application/src/main/java/com/example/application/Island.java
@@ -0,0 +1,24 @@
+package com.example.application;
+
+import javafx.scene.layout.AnchorPane;
+
+public class Island extends GameObject{
+    public Island(double x, double y, double xwidth, double ywidth) {
+        super(x, y, xwidth, ywidth);
+    }
+
+    @Override
+    public void gravityEffect() {
+
+    }
+
+    @Override
+    public void ifHeroCollides() {
+
+    }
+
+    @Override
+    public void display(AnchorPane anc) {
+
+    }
+}
